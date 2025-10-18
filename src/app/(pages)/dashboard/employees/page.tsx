83fcbe3f65d0e00717e635fe014385b458997e3b
Aadarsha Acharya <connectwithadarsha@gmail.com>
2025-10-19 01:09:44 +0545

    fix: use suspense

--- src/app/(pages)/dashboard/employees/page.tsx
+++ src/app/(pages)/dashboard/employees/page.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from "react";
 import { EmployeeManagement } from "@/modules/employees/components/employee/employee-management";
+import { Spinner } from "@/components/spinner";
 
 export default function EmployeesPage() {
-  return <EmployeeManagement />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-[calc(100vh-200px)] items-center justify-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <EmployeeManagement />
+    </Suspense>
+  );
 }
